742ddffc27092754ee69febbb6707dfc8031049b
malek lazreg <lazregmalek30@gmail.com>
2024-10-18 12:13:35 +0100

    last commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function showVideo(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'block' 
+    video.play();
+}
+function removeVideo(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'none'
+    video.onpause();
+}
+
+function removeH(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'none'
+}
+function showH(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'block'
+}
+
+
+function removeH1(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'none'
+}
+function showH1(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'block'
+}
+
+
+function removeH2(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'none'
+}
+function showH2(id){
+    var video = document.getElementById(id);
+    console.log("***********************"+video.style.display);
+    video.style.display= 'block'
+}
+
